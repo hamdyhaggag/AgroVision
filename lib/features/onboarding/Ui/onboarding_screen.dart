302cc3a092bd4836176f262a555f3d3e90bff83e
hamdy haggag <hamdyhaggag74@gmail.com>
2024-11-07 20:40:45 +0200

    fix the navigation of skip button in onboarding_screen.dart

--- lib/features/onboarding/Ui/onboarding_screen.dart
+++ lib/features/onboarding/Ui/onboarding_screen.dart
@@ -1,5 +1,4 @@
 import 'package:agro_vision/core/themes/app_colors.dart';
-import 'package:agro_vision/features/home/Ui/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
