327ecef7759b3bfe522d0fdbe62a40a58436fced
hamdy haggag <hamdyhaggag74@gmail.com>
2024-11-27 17:21:48 +0200

    normal local commit

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -43,7 +43,7 @@ class AppRouter {
         );
       case AppRoutes.createNewPassword:
         return MaterialPageRoute(
-          builder: (_) => CreateNewPassword(),
+          builder: (_) => const CreateNewPassword(),
         );
       case AppRoutes.congratulationsScreen:
         return MaterialPageRoute(
