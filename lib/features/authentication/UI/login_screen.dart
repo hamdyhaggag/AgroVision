2f13b46b5174ec9f6c99508a0d1b84dfbd58ba00
hamdy haggag <hamdyhaggag74@gmail.com>
2024-12-01 16:23:15 +0200

    feat: replace SnackBar with custom notification widget for enhanced UX

--- lib/features/authentication/UI/login_screen.dart
+++ lib/features/authentication/UI/login_screen.dart
@@ -16,87 +16,85 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: BlocListener<LoginCubit, LoginState>(
-          listener: (context, state) {
-            if (state is Success) {
-              Navigator.pushReplacementNamed(context, '/screenLayout');
-            } else if (state is Error) {
-              // Show error message
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text(state.error)),
-              );
-            }
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 60.h,
-                  ),
-                  Image.asset(
-                    'assets/images/authentication/logo4login.png',
-                    height: 250.h,
-                    width: 250.w,
-                  ),
-                  SizedBox(
-                    height: 22.h,
-                  ),
-                  const EmailAndPassword(),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 4, left: 4),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                            color: AppColors.primaryColor,
-                            fontFamily: 'SYNE',
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w500,
-                          ),
+          child: BlocListener<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is Success) {
+            Navigator.pushReplacementNamed(context, '/screenLayout');
+          } else if (state is Error) {
+            final errorMessage = state.error == 'Unauthorized'
+                ? 'Invalid email or password. Please try again.'
+                : state.error;
+
+            showCustomNotification(context, errorMessage, Colors.red);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 60.h,
+                ),
+                Image.asset(
+                  'assets/images/authentication/logo4login.png',
+                  height: 250.h,
+                  width: 250.w,
+                ),
+                SizedBox(
+                  height: 22.h,
+                ),
+                const EmailAndPassword(),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 4, left: 4),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          color: AppColors.primaryColor,
+                          fontFamily: 'SYNE',
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 12.h,
-                  ),
-                  BlocBuilder<LoginCubit, LoginState>(
-                    builder: (context, state) {
-                      if (state is Loading) {
-                        return const CircularProgressIndicator();
-                      }
-                      return CustomBottom(
-                        text: 'Log In',
-                        onPressed: () {
-                          if (kDebugMode) {
-                            print(context
-                                .read<LoginCubit>()
-                                .emailController
-                                .text);
-                          }
-                          if (kDebugMode) {
-                            print(context
-                                .read<LoginCubit>()
-                                .passwordController
-                                .text);
-                          }
-                          validateThenDoLogin(context);
-                        },
-                      );
-                    },
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 12.h,
+                ),
+                BlocBuilder<LoginCubit, LoginState>(
+                  builder: (context, state) {
+                    if (state is Loading) {
+                      return const CircularProgressIndicator();
+                    }
+                    return CustomBottom(
+                      text: 'Log In',
+                      onPressed: () {
+                        if (kDebugMode) {
+                          print(
+                              context.read<LoginCubit>().emailController.text);
+                        }
+                        if (kDebugMode) {
+                          print(context
+                              .read<LoginCubit>()
+                              .passwordController
+                              .text);
+                        }
+                        validateThenDoLogin(context);
+                      },
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
-      ),
+      )),
     );
   }
 
@@ -109,5 +107,55 @@ class LoginScreen extends StatelessWidget {
             ),
           );
     }
+  }
+
+  void showCustomNotification(
+      BuildContext context, String message, Color color) {
+    late OverlayEntry overlayEntry;
+    overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 20.w),
+            color: color,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    message,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    overlayEntry.remove();
+                  },
+                  icon: const Icon(Icons.close, color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Insert the overlay entry into the Overlay
+    Overlay.of(context).insert(overlayEntry);
+
+    // Automatically remove the notification after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      overlayEntry.remove();
+    });
   }
 }
