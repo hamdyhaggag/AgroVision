4c77248010066db08b29cd35ee9e3253f3853126
hamdy haggag <hamdyhaggag74@gmail.com>
2025-02-19 21:17:31 +0200

    Add optional parameters icon and color

--- lib/shared/widgets/custom_botton.dart
+++ lib/shared/widgets/custom_botton.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../core/themes/app_colors.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom(
-      {super.key, required this.text, this.validator, required this.onPressed});
+  const CustomBottom({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.validator,
+    this.color,
+    this.icon,
+    this.iconSize,
+    this.iconColor,
+    this.textStyle,
+  });
+
   final String text;
-  final Function(String?)? validator;
   final VoidCallback onPressed;
+  final Function(String?)? validator;
+  final Color? color;
+  final IconData? icon;
+  final double? iconSize;
+  final Color? iconColor;
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +30,61 @@ class CustomBottom extends StatelessWidget {
       width: 347.w,
       height: 48.h,
       child: MaterialButton(
-        color: AppColors.primaryColor,
+        color: color ?? AppColors.primaryColor,
         textColor: Colors.white,
-        padding: EdgeInsets.only(right: 50.h, left: 50.h),
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        elevation: 0,
+        hoverElevation: 1,
+        focusElevation: 1,
+        highlightElevation: 1,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(14),
+          borderRadius: BorderRadius.circular(14.r),
+          side: BorderSide(
+            color: color?.withOpacity(0.2) ??
+                AppColors.primaryColor.withOpacity(0.2),
+            width: 1.r,
+          ),
         ),
         onPressed: onPressed,
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500),
-        ),
+        child: _buildButtonContent(),
       ),
+    );
+  }
+
+  Widget _buildButtonContent() {
+    if (icon != null) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: iconSize ?? 20.sp,
+            color: iconColor ?? Colors.white,
+          ),
+          SizedBox(width: 8.w),
+          Flexible(
+            child: Text(
+              text,
+              style: textStyle ??
+                  TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.5,
+                  ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      );
+    }
+    return Text(
+      text,
+      style: textStyle ??
+          TextStyle(
+            fontSize: 16.sp,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.5,
+          ),
     );
   }
 }
