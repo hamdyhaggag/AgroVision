2e0d98747bb27641e2e7a500a4f35c7dbd8c4943
hamdy haggag <hamdyhaggag74@gmail.com>
2025-06-10 01:34:49 +0300

    fix(chat): update CircleAvatar background color implementation for consistency in chat UI

--- lib/features/chat/Ui/chat_screen.dart
+++ lib/features/chat/Ui/chat_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:ui' as ui;
 import 'package:agro_vision/core/themes/text_styles.dart';
-import 'package:agro_vision/features/chat/services/farmer_chat_api_service.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -129,7 +128,8 @@ class _FarmerChatScreenState extends State<FarmerChatScreen>
             ),
             child: CircleAvatar(
               radius: 20,
-              backgroundColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              backgroundColor:
+                  Theme.of(context).primaryColor.withValues(alpha: 0.1),
               child: ClipOval(
                 child: otherUserImg != null
                     ? CachedNetworkImage(
@@ -428,7 +428,7 @@ class _ChatBubbleState extends State<ChatBubble> {
                 child: CircleAvatar(
                   radius: 22,
                   backgroundColor:
-                      Theme.of(context).primaryColor.withOpacity(0.1),
+                      Theme.of(context).primaryColor.withValues(alpha: 0.1),
                   child: ClipOval(
                     child: widget.userImage != null
                         ? CachedNetworkImage(
